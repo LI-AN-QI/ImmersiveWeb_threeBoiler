e65cb71745ff5256873ccc96a44089c4d7fa4a3f
LI-AN-QI <al8286@nyu.edu>
2024-02-21 22:24:33 -0500

    update boilerplate

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ import './style.css'
 import * as THREE from 'three'
 import { addBoilerPlateMeshes, addStandardMesh } from './addMeshes'
 import { addLight } from './addLights'
-//add functions from other files
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Model from './Model'
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({antialias:true})
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100)
+const clock = new THREE.Clock()
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.08
+// controls.enablePan = false
+// controls.enableZoom = false
 const scene = new THREE.Scene()
-const meshes = {} //+ an empty object "meshes" --- store references to our meshes, store all of our meshes in there
+const meshes = {} 
+const mixers = []
 const lights = {}
 // let mesh
 
@@ -27,13 +35,26 @@ function init(){
     scene.add(meshes.default)
     scene.add(meshes.standard)
 
-    // const geometry = new THREE.BoxGeometry(1,1,1) //+ geometry
-    // const material = new THREE.MeshBasicMaterial({color:0xff0000}) //+ material
-    // mesh = new THREE.Mesh(geometry,material)//geometry + material->mesh
     camera.position.set(0,0,5)
-    // scene.add(mesh)
+  
+    instances()
     resize()
     animate()
+}
+
+function instances() {
+	const heart = new Model({
+		//4 mandatories
+        mixers: mixers,
+		url: '/heart.glb',
+        animationState:true,
+		scene: scene,
+		meshes: meshes,
+        replace: true,  //replace:url
+		name: 'heart',
+
+	})
+    heart.init()
 }
 
 function resize(){
@@ -47,6 +68,11 @@ function resize(){
 //animation
 function animate(){
     requestAnimationFrame(animate)
+    const delta = clock.getDelta()
+    for (const mixer of mixers) {
+		mixer.update(delta)
+	}
+    controls.update()
     meshes.default.rotation.x += 0.01
     meshes.default.rotation.y -= 0.01
 
